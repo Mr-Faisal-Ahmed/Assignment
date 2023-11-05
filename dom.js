c8673ff751aae00c6bb5fab7d7c621ed3c738af7
Faisal-Ahmed-Khan <51905252+Mr-Faisal-Ahmed@users.noreply.github.com>
2023-11-05 22:39:07 +0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,29 @@
+
+//document.getElementById("child c").parentElement.style.background="red";
+
+//var a = document.getElementById("child c").parentnode;
+
+
+//console.log(a);
+//var a ="please enter a valid passward"
+//alert(a)
+ //alert("Welcome to Js land....\nHappy Coding!")
+ //alert("welcome to js land")
+ 
+ //alert(document.getElementById(term)) 
+ //var a = "hello i can run js through my web browser console"
+ //console.log(a)
+ //var a = "John"
+ //var b = "my age is 15 years is old" 
+ //alert(a)
+ //alert(b) 
+//alert("Pizza \npizz\npiz \npi \np")
+//var a = "abc@gmail.com"
+//alert(a);
+//var a = 3
+//var b = 5 
+
+//alert(a*b);
+
+
+ 
